93be79dc3ee361cc1b8ef76febe991631cd3e763
andre <andre.alves@ccc.ufcg.edu.br>
2023-12-17 17:21:28 -0300

    Adding error component for loosing connection

--- src/App.js
+++ src/App.js
@@ -3,49 +3,58 @@ import ImageCard from "./components/image-card.js"
 import ImageSearch from "./components/image-search.js"
 import NoDataFoundCard from "./components/no-data-found-card.js";
 import LoadingCard from "./components/loading-card.js";
+import WifiConnectionLostComponent from "./components/wifi-connection.js";
 
 function App() {
 
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [term, setTerm] = useState("");
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo`)
     .then(res => res.json())
     .then(data => {
+      setError(null);
       setImages(data.hits);
       setIsLoading(false);
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      setError(err);
+      setIsLoading(false);
+    });
   }, [term]);
 
   return (
     <div className="container mx-auto">
+      {error ? (
+        <WifiConnectionLostComponent></WifiConnectionLostComponent>
+      ) : (
+        <div className="container">
 
-      <ImageSearch searchText={(text) => setTerm(text)}></ImageSearch>
-
-      {
-
-        !isLoading &&
-        images.length === 0 &&
-        <NoDataFoundCard></NoDataFoundCard>
-      
-      }
-
-      {
-        isLoading ?
-        <LoadingCard></LoadingCard> :
-        <div className="grid grid-cols-4 gap-4">
+          <ImageSearch searchText={(text) => setTerm(text)} />
+  
+          {!isLoading && images.length === 0 && <NoDataFoundCard/>}
+  
           {
-            images.map(image => (
-              <ImageCard key={image.id} image={image}></ImageCard>
-            ))
-          }
+            isLoading ? (
+              <LoadingCard />
+            ) : (
+              <div className="grid grid-cols-4 gap-4">
+
+                {images.map((image) => (
+                  <ImageCard key={image.id} image={image} />
+                ))}
+
+              </div>
+          )
+        }
         </div>
-    }
+      )}
     </div>
   );
+  
 }
 
 export default App;
